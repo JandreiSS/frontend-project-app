baa0461eea98ab17f9145cf6cab8928f08075b93
JandreiSS <jandreisst@gmail.com>
2020-05-06 17:11:49 -0300

    chore: List projects in browser

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
+
+import './App.css'
+
+import Header from './components/Header'
+
+export default function App(){
+  const [projects, setProjects] = useState([])
+
+  useEffect(()=>{
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+
+  function handleAddProject(){
+    // projects.push(`Novo projeto ${Date.now()}`)
+    
+    setProjects([...projects, `Novo projeto ${Date.now()}`])
+    
+  }
+
+  return (
+    <>      
+      <Header title="Projects"/>
+
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  )
+}
